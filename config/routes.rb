1511b4ffece99cf8867644bf5523cc4e104d3ae3
Tech-A25 <tech-a25@Tech-A25s-Air.lan>
2018-06-21 07:00:12 +0100

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root 'static#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :countries
+  resources :authors
+  resources :books
+  resources :chapters
+  resources :languages
+  resources :paragraphs
+end
